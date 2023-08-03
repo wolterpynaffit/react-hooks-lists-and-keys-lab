9aec256b321637b9a300baa0dc90548b2b328e97
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-08-03 02:37:10 -0400

    finished

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{links.map(link => <a href={link} key={link}>{link}</a>)}</nav>;
 }
 
 export default NavBar;
